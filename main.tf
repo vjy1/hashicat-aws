0a9999de38435a47c5dda6909d579869a4af05c0
vjy1 <vijay_chinnasamy@hotmail.com>
2020-08-02 19:49:05 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -129,12 +129,6 @@ resource aws_instance "hashicat" {
     
   }
   
-  tags = {
-    Name = "${var.prefix}-hashicat-instance"
-    #billable = true
-    Department = devops
-    
-  }
 }
 
 # We're using a little trick here so we can run the provisioner without
